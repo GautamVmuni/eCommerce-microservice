6a5a8cff06b97c46fc0d4e5e6f2c38d580d2297c
Gautam Muni <gautam.muni.13837@gmail.com>
2024-12-19 15:19:07 +0530

    First commit - customer service completed

--- services/customer/src/main/java/com/microservices/ecommerce/customer/CustomerResponse.java
+++ services/customer/src/main/java/com/microservices/ecommerce/customer/CustomerResponse.java
@@ -1,8 +1,5 @@
 package com.microservices.ecommerce.customer;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-
 public record CustomerResponse(
         String id,
         String firstName,
